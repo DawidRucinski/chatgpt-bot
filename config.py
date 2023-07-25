b20b3102657d01b1e4f1a97f2ec749df4a74bf5f
DawidRucinski <dawid_rucinski@interia.pl>
2023-07-25 14:47:55 +0200

    exported config to .env

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 
+import os
 class Config(object):
     DEBUG = True
     TESTING = False
 
 class DevelopmentConfig(Config):
-    SECRET_KEY = "this-is-a-super-secret-key"
-    OPENAI_KEY = 'provide-your-openai-key'
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 config = {
     'development': DevelopmentConfig,
